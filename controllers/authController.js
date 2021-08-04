e6d26225bdf14c4d90a74818076d48be83eb0aea
Артем Иванов <iaiw3br@gmail.com>
2021-08-04 20:25:34 +0300

    feat: comment

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,14 @@ const speakeasy = require('speakeasy');
 const bcrypt = require('bcrypt');
 const db = require('../models');
 
+/**
+ * Зарегистрировать нового пользователя
+ * @param {object} req объект запроса
+ * @param {string} req.body.name имя пользователя
+ * @param {string} req.body.password пароль пользователя
+ * @param {object} res объект ответа
+ * @returns {Promise<void>}
+ */
 const signup = async (req, res) => {
     const {name, password} = req.body;
     await db.sequelize.transaction(async transaction => {
